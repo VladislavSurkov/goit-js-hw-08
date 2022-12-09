92f801eb0114f54d42803dff8e7d0b2cbd5dabc1
VladislavSurkov <fvhabjrega@gmail.com>
2022-12-09 20:06:24 +0200

    ts 3 completed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
+const formEl = document.querySelector('.feedback-form');
 
+const dataEl = {};
+
+onFormData = e => {
+  dataEl[e.target.name] = e.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(dataEl));
+};
+
+onSubmitForm = e => {
+  e.preventDefault();
+
+  const localFeedbackForm = JSON.parse(
+    localStorage.getItem('feedback-form-state')
+  );
+  e.target.reset();
+
+  console.log(localFeedbackForm);
+  localStorage.removeItem('feedback-form-state');
+};
+
+dataFormLoad = () => {
+  const localData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+  if (localData) {
+    formEl.email.value = localData.email || '';
+    formEl.message.value = localData.message || '';
+  }
+};
+
+dataFormLoad();
+formEl.addEventListener('input', throttle(onFormData, 500));
+formEl.addEventListener('submit', onSubmitForm);
